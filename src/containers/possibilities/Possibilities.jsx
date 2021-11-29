1645a5b2db24d1e19393cc65082e11a95ac9ea71
lejodev <alejandro.rv97@gmail.com>
2021-11-29 09:51:20 -0500

    Initial commit

---
+++ src/containers/possibilities/Possibilities.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Possibilities.css"
+
+const Possibilities = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Possibilities
